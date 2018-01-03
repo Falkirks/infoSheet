fa92aab03f272bea2dbe828251bdbe00d2861fcc
Falkirks <me@falkirks.com>
2018-01-02 18:33:41 -0800

    Updates and README

--- client/src/app.js
+++ client/src/app.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Header from './components/header';
 import Home from './components/home';
+import Admin from './components/admin';
 
 
 const App = () => (
   <div>
     <Header />
     <Route exact path="/" component={Home} />
+    <Route exact path="/admin" component={Admin} />
   </div>
 );
 
---
+++ client/src/components/admin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Steps from './steps';
+
+const Admin = () => (
+  <div className="home">
+    <h1>Hi!</h1>
+  </div>
+);
+
+export default Admin;
